26e9307e751937dfa813af850a2e12d073e8b903
alex-pran <82548225+alex-pran@users.noreply.github.com>
2025-03-21 20:54:21 -0400

    add new rep and files

---
+++ motorcycle.py
@@ -0,0 +1,18 @@
+motorcycle = ['honda', 'yamaha', 'suzuki']
+print(motorcycle)
+
+motorcycle[0] = 'ducati' #make in list changes
+print(motorcycle)
+
+motorcycle.append('bmw'.upper())
+print(motorcycle)
+
+motorcycle = []
+motorcycle.append('opel')
+motorcycle.append('reno')
+motorcycle.append('fiat')
+
+print(motorcycle)
+
+motorcycle.insert(0, 'honda')
+print(motorcycle)
