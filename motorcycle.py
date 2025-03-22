5e7bd0bba3bcbdba7a8037b47c719f9ff85261a8
alex-pran <82548225+alex-pran@users.noreply.github.com>
2025-03-21 21:24:47 -0400

    add new rep and files

--- motorcycle.py
+++ motorcycle.py
@@ -16,3 +16,20 @@ print(motorcycle)
 
 motorcycle.insert(0, 'honda')
 print(motorcycle)
+
+del motorcycle[2]
+print(motorcycle)
+
+del motorcycle[1]
+print(motorcycle)
+
+motorcycle = ['honda', 'yamaha', 'suzuki']
+print(motorcycle)
+
+popped_motorcycle = motorcycle.pop(0)
+print(motorcycle)
+print(popped_motorcycle)
+
+motorcycle = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycle.pop()
+print(f'The last motorcycle I owned {last_owned.title()}')
