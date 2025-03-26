af082a782154b0e59a2583a931a39b9fdce63c79
Administrator <apranitchii@gmail.com>
2025-03-25 20:09:04 -0400

    added new file magicians.py

---
+++ magicians.py
@@ -0,0 +1,16 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+print("=========1=========")
+
+# for cat in cats:
+# for dog in dogs:
+# for item in lis_of_items:
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was amazing.")
+
+print("=========2=========")
+
