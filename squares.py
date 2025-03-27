bb0f0c6dd49ae4254ef9e86ee61ca622f52740d7
alex-pran <82548225+alex-pran@users.noreply.github.com>
2025-03-26 20:30:24 -0400

    add players.py file

--- squares.py
+++ squares.py
@@ -8,9 +8,13 @@ print(squares)
 print("==========1============")
 
 squares = []
-for value in range(3, 11, 3):
+for value in range(3, 11, 1):
     square = value ** 2
     squares.append(square)
 
 print(squares)
 
+print("==========2============")
+
+squares = [value ** 2 for value in range(1,11)]
+print(squares)
